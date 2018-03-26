7ff173974c8feb094b45c37d2fabbed6334651df
Leslie Cassert <lesliecassert@Leslies-MacBook-Pro.local>
2018-03-26 17:39:42 -0500

    finished formatting web caesar

--- main.py
+++ main.py
@@ -9,20 +9,20 @@ form = """
 <html>
     <head>
         <style>
-            form{
+            form{{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
+            }}
             
-            textarea{
+            textarea{{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -32,7 +32,7 @@ form = """
         </label>
         <br>
         <label>
-            <textarea name = "text"></textarea>
+            <textarea name = "text">{0}</textarea>
         </label>
         <br>
         <input type = "submit" />
@@ -44,13 +44,15 @@ form = """
 def encrypt():
     rot = int(request.form["rot"])
     text = request.form["text"]
-    return '<h1>' + rotate_string(text, rot) + '</h1>'
+    return form.format(rotate_string(text,rot))
+    
    
 
 
 
 @app.route("/")
 def index():
-    return form 
+    return form.format("")
+    
 
 app.run()
